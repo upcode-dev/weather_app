74ca44db6da094e0c5419ec7ea6387bd03594e37
Andruh Namolovan <andruh.namolovan@gmail.com>
2022-05-24 16:20:05 +0300

    - Fetching weather data with https://openweathermap.org/weather-conditions
    TODO:
    - toggle metric/imperial units
    - Convert project to redux

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:weather_app2/models/city.dart';
+import 'package:weather_app2/models/weather.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,8 +14,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final List<City> _cities = <City>[];
+  final List<Weather> _weather = <Weather>[];
   bool _isLoading = false;
   bool _canSearch = true;
+  bool _showWeather = false;
   final TextEditingController _controller = TextEditingController();
 
   @override
@@ -55,6 +58,40 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Future<void> _getWeather(double lat, double lon) async {
+    final Response response = await get(
+      Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=efc2113600604544cf7407bcd6ff4d8e',
+      ),
+    );
+
+    //print('${response.body}\n');
+
+    final Map<String, dynamic> result = jsonDecode(response.body) as Map<String, dynamic>;
+
+    final int id = result['weather'][0]['id'] as int;
+    //print(id);
+    //print(result['weather'][0]['id']);
+    final String main = result['weather'][0]['main'] as String;
+    final String description = result['weather'][0]['description'] as String;
+    final String icon = result['weather'][0]['icon'] as String;
+    final double temp = result['main']['temp'] as double;
+    final double feelsLike = result['main']['feels_like'] as double;
+    final double tempMin = result['main']['temp_min'] as double;
+    final double tempMax = result['main']['temp_max'] as double;
+    final int pressure = result['main']['pressure'] as int;
+    final int humidity = result['main']['humidity'] as int;
+    final double windSpeed = result['wind']['speed'] as double;
+
+    final Weather weather = Weather(id: id, main: main, description: description, icon: icon, temp: temp, feelsLike: feelsLike, tempMin: tempMin, tempMax: tempMax, pressure: pressure, humidity: humidity, windSpeed: windSpeed);
+    //print(weather);
+
+    setState(() {
+      _weather.add(weather);
+      _showWeather = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,6 +100,35 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Builder(
         builder: (BuildContext context) {
+          if (_showWeather) {
+            return AlertDialog(
+              title: const Text('Weather'),
+              content: Column(
+                children: <Widget>[
+                  Text('ID: ${_weather[0].id}'),
+                  Text('main: ${_weather[0].main}'),
+                  Text('name: ${_weather[0].description}'),
+                  Text('icon: ${_weather[0].icon}'),
+                  Text('temp: ${_weather[0].temp}'),
+                  Text('feelsLike: ${_weather[0].feelsLike}'),
+                  Text('tempMin: ${_weather[0].tempMin}'),
+                  Text('tempMax: ${_weather[0].tempMax}'),
+                  Text('pressure: ${_weather[0].pressure}'),
+                  Text('humidity: ${_weather[0].humidity}'),
+                  Text('windSpeed: ${_weather[0].windSpeed}'),
+                ],
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    _showWeather = false;
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          }
           if (_canSearch) {
             return Column(
               children: <Widget>[
@@ -104,14 +170,19 @@ class _HomePageState extends State<HomePage> {
                   itemBuilder: (BuildContext context, int index) {
                     final City city = _cities[index];
 
-                    return Column(
-                      children: <Widget>[
-                        Text('City name: ${city.name}'),
-                        Text('Country code: ${city.country}'),
-                        Text('latitude: ${city.lat}'),
-                        Text('longitude: ${city.lon}'),
-                        const SizedBox(height: 10),
-                      ],
+                    return GestureDetector(
+                      onTap: () {
+                        _getWeather(city.lat, city.lon);
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Text('City name: ${city.name}'),
+                          Text('Country code: ${city.country}'),
+                          Text('latitude: ${city.lat}'),
+                          Text('longitude: ${city.lon}'),
+                          const SizedBox(height: 10),
+                        ],
+                      ),
                     );
                   },
                 ),
